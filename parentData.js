8e91e96c05f43b066e3f694db920566665f65e13
mbbluestalker <olawole@bepeerless.co>
2024-02-27 14:33:36 +0100

    parent data

---
+++ parentData.js
@@ -0,0 +1,22 @@
+export const tabs = [
+    {
+      title: "Assets",
+      code: "ASST",
+    },
+    {
+      title: "Liabilities",
+      code: "LIA",
+    },
+    {
+      title: "Equities",
+      code: "EQT",
+    },
+    {
+      title: "Revenue",
+      code: "REV",
+    },
+    {
+      title: "Expense",
+      code: "EXP",
+    },
+  ];
